5fa36e5af1616b5ceafd925d0d948828f8f164b9
eliashaz <e.hazbon@gmail.com>
2020-06-25 21:50:54 +0200

    added checking of certificate expiry feature

--- src/proxy.py
+++ src/proxy.py
@@ -1,10 +1,28 @@
 import tornado.ioloop
 import tornado.web
 import tornado.httpclient
+import os, datetime, subprocess
+from urllib.parse import urlparse
 
 class MainHandler(tornado.web.RequestHandler):
     async def get(self):
-        target = self.get_argument('target', None)        
+        target = self.get_argument('target', None)
+        network_location = str(urlparse(target).netloc)
+        now = datetime.datetime.now()
+        if(network_location not in ledger or ledger[network_location]["lasttick"] < now - datetime.timedelta(days=1)):
+            #command = "echo | openssl s_client -showcerts -connect " + network_location + " -proxy " + proxy_host + ":" + proxy_port + " 2>/dev/null | openssl x509 -enddate --noout"
+            proxy_string = proxy_host + ":" + proxy_port
+            p1 = subprocess.Popen(["echo"], stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(["openssl", "s_client", "-showcerts", "-connect", network_location, "-proxy", proxy_string], stdin=p1.stdout, stdout=subprocess.PIPE)
+            p3 = subprocess.Popen(["openssl", "x509", "-enddate", "--noout"], stdin=p2.stdout, stdout=subprocess.PIPE)
+            p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+            p2.stdout.close()
+            output = p3.communicate()[0].decode()
+            date_string = (re.split('=',output)[1]).rstrip("\n")
+            date_format = "%b %d %H:%M:%S %Y %Z"
+            date = datetime.datetime.strptime(date_string, date_format)
+            expiry = (date - now).days
+            ledger[network_location] = {"lasttick":now, "expiry":expiry}
         http_client = tornado.httpclient.AsyncHTTPClient()
         try:
             request = tornado.httpclient.HTTPRequest(url=str(target), \
@@ -19,8 +37,7 @@ class MainHandler(tornado.web.RequestHandler):
         except Exception as e:
             self.write(str(e))
         else:
-            self.write(response.body)
-        
+            self.write({"original_response":str(response.body), "ssl_cert_expiry":ledger[network_location]["expiry"]})
 
 def make_app():
     return tornado.web.Application([
@@ -28,10 +45,12 @@ def make_app():
     ])
 
 if __name__ == "__main__":
-    import os, re
+    import re
     proxy = os.getenv('HTTP_PROXY')
+    global ledger
+    ledger = {}
     splits = re.split('/|:|@',proxy)
-    global proxy_username, proxy_password, proxy_host, proxy_port
+    global proxy_username, proxy_password, proxy_host, proxy_port, timer
     proxy_username = splits[3]
     proxy_password = splits[4]
     proxy_host = splits[5]
