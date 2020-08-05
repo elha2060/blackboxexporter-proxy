a250cf5bfd861812c70977bd64dc1eeff41c04f1
eliashaz <e.hazbon@gmail.com>
2020-08-05 18:58:39 +0200

    removed certificate check added post request possibility

--- src/proxy.py
+++ src/proxy.py
@@ -1,7 +1,6 @@
 import tornado.ioloop
 import tornado.web
 import tornado.httpclient
-import os, datetime, subprocess
 from urllib.parse import urlparse
 import re
 
@@ -14,28 +13,10 @@ class MainHandler(tornado.web.RequestHandler):
         proxy_password = splits[1]
         proxy_host = splits[2]
         proxy_port = splits[3]
-        network_location = str(urlparse(target).netloc)
-        now = datetime.datetime.now()
-        if(network_location not in ledger or ledger[network_location]["lasttick"] < now - datetime.timedelta(days=1)):
-            proxy_string = proxy_host + ":" + proxy_port
-            command = "echo | openssl s_client -showcerts -connect " + network_location + " -servername " + network_location + " -proxy " + proxy_string + " 2>/dev/null | openssl x509 -enddate --noout"
-            try:
-                p = subprocess.run(command, shell=True, timeout=5, capture_output=True)
-            except Exception as e:
-                print(str(e))
-            else:
-                output = p.stdout.decode().rstrip("\n")
-                if (p.stderr):
-                    print(p.stderr.decode())
-                else:
-                    date_string = (re.split('=',output)[1])
-                    date_format = "%b %d %H:%M:%S %Y %Z"
-                    date = datetime.datetime.strptime(date_string, date_format)
-                    expiry = (date - now).days
-                    ledger[network_location] = {"lasttick":now, "expiry":expiry}
-        http_client = tornado.httpclient.AsyncHTTPClient()
+        http_client = tornado.httpclient.AsyncHTTPClient()        
         try:
-            request = tornado.httpclient.HTTPRequest(url=str(target), \
+            request = tornado.httpclient.HTTPRequest(method="GET",
+                    url=str(target), \
                     connect_timeout=3, \
                     proxy_host=proxy_host, \
                     proxy_port=int(proxy_port), \
@@ -47,7 +28,38 @@ class MainHandler(tornado.web.RequestHandler):
         except Exception as e:
             self.write(str(e))
         else:
-            self.write({"original_response":str(response.body), "ssl_cert_expiry":ledger[network_location]["expiry"]})
+            self.write(str(response.body))
+    
+    async def post(self):
+        target = self.get_argument('target', None)
+        proxy = self.get_argument('proxy', None)
+        splits = re.split(':|@',str(proxy))
+        proxy_username = splits[0]
+        proxy_password = splits[1]
+        proxy_host = splits[2]
+        proxy_port = splits[3]
+        headers = self.request.headers
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        try:
+            request = tornado.httpclient.HTTPRequest(method="POST",
+                    url=str(target), \
+                    body=self.request.body, \
+                    headers=headers, \
+                    connect_timeout=3, \
+                    proxy_host=proxy_host, \
+                    proxy_port=int(proxy_port), \
+                    proxy_username=proxy_username, \
+                    proxy_password=proxy_password, \
+                    validate_cert=False, \
+                    allow_ipv6=False)
+            response = await http_client.fetch(request)
+        except Exception as e:
+            self.write(str(e))
+        else:
+            if response.headers['Content-Type'] == 'application/json':
+                self.write(tornado.escape.json_decode(response.body))
+            else:
+                self.write(str(response.body))
 
 def make_app():
     return tornado.web.Application([
